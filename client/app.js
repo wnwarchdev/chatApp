c237b0a74b95ca6d608b8c1f02fdd506c7cffdca
Wojtek Nowak <billnewmann@gmail.com>
2020-06-26 23:00:54 +0100

    Add userName variable

--- client/app.js
+++ client/app.js
@@ -6,3 +6,8 @@ const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
+
+
+///GLOBAL VARIABLES
+
+let userName
