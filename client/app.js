4df91f4dbf73381da8c920463474f559998acee7
Wojtek Nowak <billnewmann@gmail.com>
2020-06-27 16:31:54 +0100

    Add join button functionality; add webpack

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 ///CONST  
 
-const loginFrom = document.getElementById('welcome-form');
+const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
@@ -10,4 +10,26 @@ const messageContentInput = document.getElementById('message-content');
 
 ///GLOBAL VARIABLES
 
-let userName
+let userName;
+//console.log('user name is: ',userName);
+//console.log ('loginForm is:', loginForm )
+
+
+/// LOGIN
+
+function login(event){
+    event.preventDefault();
+    if (userNameInput.value === '' || userNameInput.value === undefined ){
+        alert('no username')
+    } else {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    }
+}
+
+
+
+/// LISTENERS
+
+loginForm.addEventListener('submit', login);
