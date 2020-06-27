07c7d26eee59e56bade33fcdb025c938e1aee51a
Wojtek Nowak <billnewmann@gmail.com>
2020-06-27 17:04:29 +0100

    Add sendMessage and add Message functions; remove proxy messages from html

--- client/app.js
+++ client/app.js
@@ -29,7 +29,35 @@ function login(event){
 }
 
 
+function sendMessage(event) {
+    event.preventDefault();
+    if (messageContentInput.value === '' || messageContentInput.value === undefined) {
+      alert('no message');
+    } else {
+      addMessage(userName, messageContentInput.value);
+      messageContentInput.value = '';
+    }
+  }
+
+
+
+function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+  }
+
+
 
 /// LISTENERS
 
 loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
